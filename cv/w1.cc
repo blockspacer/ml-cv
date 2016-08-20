a98b5613b41df9b7e73f6f447703362e6e301e67
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-08-20 20:43:45 +0300

    select contours

--- cv/w1.cc
+++ cv/w1.cc
@@ -1,33 +1,83 @@
-//#include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <opencv2/opencv.hpp>
 
-int main( int argc, char** argv ) 
-{
-        // задаём высоту и ширину картинки
-        int height = 620;
-        int width = 440;
-        // задаём точку для вывода текста
-        CvPoint pt = cvPoint( height/4, width/2 );
-        // Создаёи 8-битную, 3-канальную картинку
-        IplImage* hw = cvCreateImage(cvSize(height, width), 8, 3);
-        // заливаем картинку чёрным цветом
-        cvSet(hw,cvScalar(0,0,0));
-        // инициализация шрифта
-        CvFont font;
-        cvInitFont( &font, CV_FONT_HERSHEY_COMPLEX,1.0, 1.0, 0, 1, CV_AA);
-        // используя шрифт выводим на картинку текст
-        cvPutText(hw, "OpenCV Step By Step", pt, &font, CV_RGB(150, 0, 150) );
+#include <iostream>
+#include <ctime>
 
-        // создаём окошко
-        cvNamedWindow("Hello World", 0);
-        // показываем картинку в созданном окне
-        cvShowImage("Hello World", hw);
-        // ждём нажатия клавиши
-        cvWaitKey(0);
-        
-        // освобождаем ресурсы
-        cvReleaseImage(&hw);
-        cvDestroyWindow("Hello World");
-        return 0;
+using namespace cv;
+using namespace std;
+
+using namespace std;
+
+void shape( Mat& m )
+{
+	cout << "(" << m.rows << "," << m.cols << ")" << endl;
+}
+
+void dt( clock_t begin, clock_t end )
+{
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	cout << elapsed_secs << endl;
+}
+
+int main( int argc, char** argv )
+{
+	clock_t begin;
+
+	// Load the source image
+	Mat src =
+	        cv::imread(
+	                "/home/zaqwes/Dropbox/Pictures/the_composer_by_stellarstatelogic-d5srz6u.png",
+//	                "/home/zaqwes/Dropbox/Pictures/84afdc887c2e0bb6507cb8e79fca6aae.jpg",
+	                0);
+	shape(src);
+
+	// Create a destination Mat object
+	Size s = src.size();
+	Mat dst((int) (s.height * 0.3), (int) (s.width * 0.3),
+	        DataType<uint8_t>::type);
+	Size d = dst.size();
+
+	// Resize
+	begin = clock();
+	resize(src, dst, dst.size());
+	dt(begin, clock());
+	shape(dst);
+
+	// Smooth
+	begin = clock();
+	medianBlur(dst, src, 3);
+	dt(begin, clock());
+
+	// Thresh.
+	begin = clock();
+	adaptiveThreshold(src, dst, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,
+	        35, -5);
+	dt(begin, clock());
+
+	// Postprocessing
+	int dilation_size = 1;
+	Mat element = getStructuringElement(MORPH_RECT,
+	        Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+	        Point(dilation_size, dilation_size));
+	// Apply the dilation operation
+	begin = clock();
+	dilate(dst, src, element);
+	dt(begin, clock());
+
+	// Countours
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	begin = clock();
+	findContours(src, contours, hierarchy, CV_RETR_CCOMP,
+	        CV_CHAIN_APPROX_TC89_L1);
+	dt(begin, clock());
+	cout << contours.size() << endl;
+
+	// Draw
+	namedWindow("Before", CV_WINDOW_AUTOSIZE);
+	// display the source image
+	imshow("Before", src);
+	waitKey();
+	return 0;
 }
