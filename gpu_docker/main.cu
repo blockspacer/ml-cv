f37ccf928acd88b520fc4aa13c2c6f61bc4593cf
zaqwes8811 <igor.a.lugansky@gmail.com>
2017-07-23 21:55:25 +0300

    stereo: census

--- gpu_docker/main.cu
+++ gpu_docker/main.cu
@@ -4,6 +4,7 @@
 
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <stdio.h>
 
 #include <stdint.h>
@@ -48,7 +49,8 @@ __device__ int clamp_2d_position(int w, int h, int gx, int gy) {
 }
 
 __global__
-void cencus(const uchar* in, uint32_t* out, int w, int h) {
+void cencus_i3_xsobel5(const uchar* in, const uchar* in_xsobel, uint32_t* out,
+		int w, int h) {
 	int gx = blockIdx.x * blockDim.x + threadIdx.x;
 	int gy = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -84,31 +86,93 @@ void cencus(const uchar* in, uint32_t* out, int w, int h) {
 
 // [0, 32]
 #define MAX_DISP 32
-#define WS 7
+#define WS 11
+
+#define SHIFT 4
+#define FILTERED ((-1) << SHIFT)
 
 __global__ void sbm_census(uint32_t* i0, uint32_t* i1, int w, int h,
-		short* d_disp) {
-	int gx = blockIdx.x * blockDim.x + threadIdx.x;
-	int gy = blockIdx.y * blockDim.y + threadIdx.y;
+		short* d_disp, int ws) {
+	int gx_0 = blockIdx.x * blockDim.x + threadIdx.x;
+	int gy_0 = blockIdx.y * blockDim.y + threadIdx.y;
 
-	int pos = clamp_2d_position(w, h, gx, gy);
-	if (pos < 0) {
+	int pos_0 = clamp_2d_position(w, h, gx_0, gy_0);
+	if (pos_0 < 0) {
 		return;
 	}
+
+	d_disp[pos_0] = FILTERED;
 
 	// fixme: можно кстати искать на меньшем диапазоне, а не выкидывать
-	if (gx < MAX_DISP) {
+	if (gx_0 < MAX_DISP) {
 		return;
 	}
 
-	short* energy[MAX_DISP];
-	uint32_t i0_bs = i0[pos];
+	short shd[MAX_DISP];
+	int hbs = ws >> 1;
 
-	// for ...
+	// SAD -> SHD (Sum of Hamming Distance (SHD))
+	// WTA strategy
 
+	// fixme: preload window - may be bad idia
+
+	// (-max, 0]
+	for (int d = 0; d < MAX_DISP; ++d) {
+		int gx_1 = gx_0 - d;
+		int gy_1 = gy_0;
+
+		int shd_acc = 0;
+
+		for (int x = -hbs; x < hbs; ++x) {
+			for (int y = -hbs; y < hbs; ++y) {
+				int ax0 = clamp(gx_0 + x, 0, w);
+				int ay0 = clamp(gy_0 + y, 0, h);
+
+				int ax1 = clamp(gx_1 + x, 0, w);
+				int ay1 = clamp(gy_1 + y, 0, h);
+
+				uint32_t I0 = i0[ay0 * w + ax0];
+				uint32_t I1 = i1[ay1 * w + ax1];
+
+				uint32_t dI = I0 ^ I1;
+
+				// slow
+				shd_acc += __popc(dI);
+//				shd_acc += I0 + I1;
+
+			}
+		}
+
+		shd[d] = shd_acc;
+	}
+
+	// minarg
+	float minval = 1e6;
+	int arg = -1;
+	for (int d = 0; d < MAX_DISP; ++d) {
+		if (minval > shd[d]) {
+			arg = d;
+			minval = shd[d];
+		}
+	}
+
+	if (arg == -1) {
+		d_disp[pos_0] = FILTERED;
+		return;
+	}
+
+	// subpixel
+
+	// uniqueless
+
+	// text. thr.
+
+	// store
+	d_disp[pos_0] = arg << SHIFT;
 }
 
-void census(uchar* d_img, uint32_t* d_img_census, int w, int h) {
+void census(uchar* d_img, uchar* d_img_xsobel, uint32_t* d_img_census, int w,
+		int h) {
 	const dim3 wg(w / 16 + 1, h / 16 + 1);
 	const dim3 bs(16, 16);
 	// measure
@@ -120,7 +184,7 @@ void census(uchar* d_img, uint32_t* d_img_census, int w, int h) {
 	cudaEventRecord(start);
 	// Perform SAXPY on 1M elements
 
-	cencus<<<wg, bs>>>(d_img, d_img_census, w, h);
+	cencus_i3_xsobel5<<<wg, bs>>>(d_img, d_img_xsobel, d_img_census, w, h);
 
 	gpuErrchk(cudaPeekAtLastError());
 	gpuErrchk(cudaDeviceSynchronize());
@@ -134,9 +198,10 @@ void census(uchar* d_img, uint32_t* d_img_census, int w, int h) {
 	cout << "elapsed:" << milliseconds / 1e3 << endl;
 }
 
-struct gpu_img_triple_t {
+struct gpu_matching_handle_t {
 
-	gpu_img_triple_t(int N, uchar* h_i0, uchar* h_i1) {
+	gpu_matching_handle_t(int N, uchar* h_i0, uchar* h_i1, uchar* h_i0_xsobel,
+			uchar* h_i1_xsobel) {
 		d_i0 = 0;
 		d_i0_census = 0;
 		d_i1 = 0;
@@ -162,17 +227,21 @@ struct gpu_img_triple_t {
 		gpuErrchk(cudaMalloc(&d_disp_i16, N * sizeof(short)));
 	}
 
-	~gpu_img_triple_t() {
+	~gpu_matching_handle_t() {
 		gpuErrchk(cudaFree(d_i0));
+		gpuErrchk(cudaFree(d_i0_xsobel));
 		gpuErrchk(cudaFree(d_i0_census));
 		gpuErrchk(cudaFree(d_i1));
+		gpuErrchk(cudaFree(d_i1_xsoble));
 		gpuErrchk(cudaFree(d_i1_census));
 		gpuErrchk(cudaFree(d_disp_i16));
 	}
 
 	uchar *d_i0;
+	uchar *d_i0_xsobel;
 	uint32_t *d_i0_census;
 	uchar *d_i1;
+	uchar *d_i1_xsoble;
 	uint32_t *d_i1_census;
 
 	short* d_disp_i16;
@@ -183,20 +252,27 @@ int main(void) {
 	Mat im0 = imread("im0.png", 0);
 	Mat im1 = imread("im1.png", 0);
 
+	Mat dst0, dst1;
+	double f = 0.2;
+	cv::resize(im0, dst0, Size(0, 0), f, f);
+	im0 = dst0;
+	cv::resize(im1, dst1, Size(0, 0), f, f);
+	im1 = dst1;
+
 	int w = im0.cols;
 	int h = im0.rows;
 	int N = w * h;
 
-	gpu_img_triple_t gpair(N, im0.data, im1.data);
+	gpu_matching_handle_t mhandle(N, im0.data, im1.data, 0, 0);
 
 	// census
 	vector<uint32_t> h_i0_census(N);
 	vector<uint32_t> h_i1_census(N);
-	census(gpair.d_i0, gpair.d_i0_census, w, h);
+	census(mhandle.d_i0, mhandle.d_i0_xsobel, mhandle.d_i0_census, w, h);
 
 	gpuErrchk(
-			cudaMemcpy(&h_i0_census[0], gpair.d_i0_census, N * sizeof(uint32_t),
-					cudaMemcpyDeviceToHost));
+			cudaMemcpy(&h_i0_census[0], mhandle.d_i0_census,
+					N * sizeof(uint32_t), cudaMemcpyDeviceToHost));
 
 	// https://stackoverflow.com/questions/14581806/can-not-use-cv-32uc1
 	Mat A = Mat(h, w, CV_32S, &h_i0_census[0]);
@@ -205,10 +281,10 @@ int main(void) {
 	A.convertTo(B, CV_8U);
 	imwrite("out0.png", B);
 
-	census(gpair.d_i1, gpair.d_i1_census, w, h);
+	census(mhandle.d_i1, mhandle.d_i0_xsobel, mhandle.d_i1_census, w, h);
 	gpuErrchk(
-			cudaMemcpy(&h_i1_census[0], gpair.d_i1_census, N * sizeof(uint32_t),
-					cudaMemcpyDeviceToHost));
+			cudaMemcpy(&h_i1_census[0], mhandle.d_i1_census,
+					N * sizeof(uint32_t), cudaMemcpyDeviceToHost));
 
 	A = Mat(h, w, CV_32S, &h_i1_census[0]);
 
@@ -217,6 +293,7 @@ int main(void) {
 
 	// matching
 	{
+		vector<short> h_disp(N, 0);
 		const dim3 wg(w / 16 + 1, h / 16 + 1);
 		const dim3 bs(16, 16);
 		// measure
@@ -227,8 +304,8 @@ int main(void) {
 
 		cudaEventRecord(start);
 
-		sbm_census<<<wg, bs>>>(gpair.d_i0_census, gpair.d_i1_census, w, h,
-				gpair.d_disp_i16);
+		sbm_census<<<wg, bs>>>(mhandle.d_i0_census, mhandle.d_i1_census, w, h,
+				mhandle.d_disp_i16, WS);
 
 		gpuErrchk(cudaPeekAtLastError());
 		gpuErrchk(cudaDeviceSynchronize());
@@ -240,6 +317,16 @@ int main(void) {
 		cudaEventElapsedTime(&milliseconds, start, stop);
 
 		cout << "elapsed:" << milliseconds / 1e3 << endl;
+
+		// store
+		gpuErrchk(
+				cudaMemcpy(&h_disp[0], mhandle.d_disp_i16, N * sizeof(short),
+						cudaMemcpyDeviceToHost));
+
+		A = Mat(h, w, CV_16S, &h_disp[0]);
+
+		A.convertTo(B, CV_8U);
+		imwrite("d_disp.png", B);
 	}
 
 }
