5a04244e6792f43fedb789e633731e4b2cda5d01
zaqwes8811 <igor.a.lugansky@gmail.com>
2019-03-02 15:52:10 +0300

    upd

--- mem_disk_storage.py
+++ mem_disk_storage.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import hashlib
+from pandas import HDFStore
 import md5
 
 from datetime import datetime
@@ -24,11 +25,21 @@ if __name__ == '__main__':
     # df = pd.DataFrame(np.random.randn(0, 0), columns=list('ABCD'))
     # print df
 
+
+    # https://github.com/pandas-dev/pandas/issues/14655
     # Empty dataframe
-    df = pd.DataFrame(columns=['timeticket', 'hash_val', 'h_m'])
+    # https://pbpython.com/pandas_dtypes.html
+
+    dtypes = {'timeticket': 'datetime64[ns]',
+              'hash_val': 'object',  # str the same?
+              'h_m': 'float64'}
+    columns = ['timeticket', 'hash_val', 'h_m']
 
     # Append
-    for i in range(16):
+    # FIXME: faster!
+    # https://stackoverflow.com/questions/22998859/hdfstore-with-string-columns-gives-issues
+    vec = []
+    for i in range(500):
         # https://stackoverflow.com/questions/44513738/pandas-create-empty-dataframe-with-only-column-names/44514187
         # timeticket = datetime.now()
         # timeticket = datetime.now()
@@ -42,12 +53,60 @@ if __name__ == '__main__':
         timeticket = s_dt + us_dt
         # print datetime.datetime.combine(s_dt, us_dt)
 
-        df = df.append({'timeticket': timeticket, 'hash_val': hashlib.md5('black' + str(i)).hexdigest(),
-                        'h_m': 5.0 + np.random.rand()},
-                       ignore_index=True)
+        # FIXME: Recreation! bad
+        row = {'timeticket': timeticket, 'hash_val': hashlib.md5('black' + str(i)).hexdigest(),
+               'h_m': 5.0 + np.random.rand()}
+        vec.append(row)
 
+        # Very slow
+        # df = df.append(},
+        #                ignore_index=True)
+
+    df = pd.DataFrame(vec, columns=columns)  # )
+    for c in df.columns:
+        df[c] = df[c].astype(dtypes[c])
+
+    print df.info()
+    # print df.dtypes
+    print 'Store...'
+    # print df
+
+    # Append to hdf5 - no it's better to recreate!
+    fn = '/tmp/data.h5'
+    store = HDFStore(fn)
+    # adding dataframe to the HDF5 file
+    # store.put('d1', df, format='table', data_columns=True)
+
+    # , data_columns=True
+    # store['df'] = df  # save it
+    # store.put('d1', df, data_columns=True)  # no chunks
+    store.put('df', df, data_columns=True, format='table')
     print df
 
-    # Append to hdf5
-
+    store.close()
+    # store['df']  # load it
+    #
     # Read hdf5 by chunks
+    # https://towardsdatascience.com/why-and-how-to-use-pandas-with-large-data-9594dda2ea4c
+    # https://stackoverflow.com/questions/40348945/reading-data-by-chunking-with-hdf5-and-pandas
+    rd_store = HDFStore(fn)
+    #
+    # df = pd.DataFrame(columns=columns)
+    # chunksize = 4096
+    # # %%timeit
+    # # for chunk in pd.read_hdf(fn, 'df', chunksize=chunksize, where='h_m < 5.3'):
+    # #     df = pd.concat([df, chunk], ignore_index=True)
+    #
+    # # sel by time
+    # # https://stackoverflow.com/questions/25681308/pandas-read-hdf-query-by-date-and-time-range
+    # # Может лучше не таблицей хранить если выбирать по времени
+    c = rd_store.select_column('df', 'timeticket')
+
+    where = pd.DatetimeIndex(c).indexer_between_time('12:00', '16:56')
+    #
+    resp = rd_store.select('df', where=where)
+    # print resp.info()
+    print resp
+
+    # Another selection
+    # https://stackoverflow.com/questions/20502996/use-or-in-hdfstore-select-pandas
